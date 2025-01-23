812182e08e36364bc992a41cdc2034d67c824111
Lalit Joshi <lalitjoshihld9@gmail.com>
2025-01-23 18:14:05 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "lalit"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number (score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// "33"  => 33
+// "33ac" => NaN
+// true => 1; fls => 0;
+
+let isLoggedIn = 1
+
+let booleanIsLooggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLooggedIn);
+
+// 1 => true; 0=> false
+// "" =>  false
+// "lalit" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
