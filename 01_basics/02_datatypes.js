eaef4f54eccdcd8ea52a3a467e60f5f80ccfe82b
Lalit Joshi <lalitjoshihld9@gmail.com>
2025-01-23 13:46:59 +0530

    added datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use Strict"; // treat all JS code as newer version
+
+//alert(3+3)  // we are usig node.js not browser.
+
+let name ="lalit"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint =>
+// string => ""
+// boolean => true , false
+// null => standalone value
+// undefied => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
